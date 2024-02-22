c553de37ff7471f2582158864003fd94cd2bf513
DASOM <n980805@gmail.com>
2024-02-22 11:06:38 +0900

    feat: 회원 정보 수정과 삭제 api 추가

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,16 +1,11 @@
 package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +18,14 @@ public class UserController {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    // 회원 등록
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
         String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
         jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
+    // 회원 정보 전체 조회
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
         List<UserResponse> responses = new ArrayList<>();
@@ -41,4 +38,41 @@ public class UserController {
             return new UserResponse(id, name, age);
         });
     }
+
+    // 회원 정보 수정
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        // 사용자 존재 여부 확인
+        String readSql = "SELECT * FROM user WHERE id = ?";
+
+        // DB에 데이터 여부 확인
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+
+        // 조회에 실패 하면 예외 발생
+        if (isUserNotExist) {
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    // 회원 정보 삭제
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        // 사용자 존재 여부 확인
+        String readSql = "SELECT * FROM user WHERE name = ?";
+
+        // DB에 데이터 여부 확인
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+
+        // 조회에 실패 하면 예외 발생
+        if (isUserNotExist) {
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+
 }
