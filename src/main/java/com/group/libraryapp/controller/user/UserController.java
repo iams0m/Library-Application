da16b0f861f5de470762b11b94a3ab3b2d05c975
DASOM <n980805@gmail.com>
2024-02-23 17:21:40 +0900

    Refactor: 컨트롤러 layer 분리 (service, repository 추가)

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,67 +13,35 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final UserService userService;
 
     public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.userService = new UserService(jdbcTemplate);
     }
 
     // 회원 등록
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
+        userService.saveUser(request);
     }
 
     // 회원 정보 전체 조회
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
         List<UserResponse> responses = new ArrayList<>();
-
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
     }
 
     // 회원 정보 수정
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request) {
-        // 사용자 존재 여부 확인
-        String readSql = "SELECT * FROM user WHERE id = ?";
-
-        // DB에 데이터 여부 확인
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-
-        // 조회에 실패 하면 예외 발생
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE user SET name = ? WHERE id = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
+        userService.updateUser(request);
     }
 
     // 회원 정보 삭제
     @DeleteMapping("/user")
     public void deleteUser(@RequestParam String name) {
-        // 사용자 존재 여부 확인
-        String readSql = "SELECT * FROM user WHERE name = ?";
-
-        // DB에 데이터 여부 확인
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-
-        // 조회에 실패 하면 예외 발생
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "DELETE FROM user WHERE name = ?";
-        jdbcTemplate.update(sql, name);
+        userService.deleteUser(name);
     }
 
 }
---
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -0,0 +1,46 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserService {
+
+    public UserService(JdbcTemplate jdbcTemplate) {
+        userRepository = new UserRepository(jdbcTemplate);
+    }
+
+    private final UserRepository userRepository;
+
+    public void saveUser(UserCreateRequest request) {
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers() {
+        return userRepository.getUsers();
+    }
+
+    public void updateUser(UserUpdateRequest request) {
+
+        // UserRepository 연결
+        // 1. 사용자 존재 여부 확인
+        if (userRepository.isUserNotExist(request.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        // 2. 사용자 이름 수정
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
+
+    public void deleteUser(String name) {
+        if (userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.deleteUser(name);
+    }
+}
