1226d046f4110cdbf05b360f6f7bc69378d7ffc5
DASOM <n980805@gmail.com>
2024-03-02 01:48:44 +0900

    feat: cascade, orphanRemoval 옵션 적용

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -18,7 +18,7 @@ public class User {
 
     private Integer age;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserLoanHistory> userLoanHistorys = new ArrayList<>();
 
     protected User() {
